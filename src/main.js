190ee70d9ab08ea594765770232e7580c35e5208
Franco Cusmai <fcusmai@b2gov.com>
2022-04-08 14:03:47 -0300

    first commit

---
+++ src/main.js
@@ -0,0 +1,27 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import i18n from './i18n'
+
+// Bootstrap config
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import './app.scss'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+// Plugins
+import ThemeSwitch from '@/plugins/theme-switch'
+
+Vue.use(ThemeSwitch)
+
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  i18n,
+  render: h => h(App)
+}).$mount('#app')
